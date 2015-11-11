c1101b73a92e465a1d88131e1ae294d73f6e26f8
Alec Fenichel <alec.fenichel@gmail.com>
2015-11-11 13:02:01 -0500

    Setup test coverage

---
+++ tests/blanket-options.js
@@ -0,0 +1,25 @@
+/* globals blanket, module */
+
+var options = {
+  modulePrefix: 'gtfriender-web-interface',
+  filter: '//.*gtfriender-web-interface/.*/',
+  antifilter: '//.*(tests|template).*/',
+  loaderExclusions: [],
+  enableCoverage: true,
+  cliOptions: {
+    reporters: ['lcov'],
+    autostart: true,
+    lcovOptions: {
+      outputFile: 'lcov.info',
+      renamer: function(moduleName){
+        var expression = /^gtfriender-web-interface/;
+        return moduleName.replace(expression, './app') + '.js';
+      }
+    }
+  }
+};
+if (typeof exports === 'undefined') {
+  blanket.options(options);
+} else {
+  module.exports = options;
+}
