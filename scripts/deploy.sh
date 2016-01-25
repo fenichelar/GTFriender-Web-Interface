d3d7a99dfc9a2c93c2a298b14d4e40146b739299
Alec Fenichel <alec.fenichel@gmail.com>
2016-01-24 23:52:12 -0500

    Deploy only on master branch but build on all

---
+++ scripts/deploy.sh
@@ -0,0 +1,5 @@
+ember build --environment production
+openssl aes-256-cbc -K $encrypted_faf5e7d2acb0_key -iv $encrypted_faf5e7d2acb0_iv -in travis_rsa.enc -out travis_rsa -d
+chmod 600 travis_rsa
+mv travis_rsa ~/.ssh/id_rsa
+scp -P $SCP_PORT -o stricthostkeychecking=no -r ./dist/. $SCP_USER@$SCP_IP:/var/www/gtfriender.com/.
