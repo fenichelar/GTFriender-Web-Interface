5efa301d5132e53086f78d22ed9ac6405327c297
Alec Fenichel <alec.fenichel@gmail.com>
2016-04-17 13:28:34 -0400

    Use link-to links

--- app/routes/categories/category/activities/activity/events.js
+++ app/routes/categories/category/activities/activity/events.js
@@ -3,18 +3,22 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     var model = {};
-    var category = this.modelFor('categories.category').get('id');
-    var activity = this.modelFor('categories.category.activities.activity').get('id');
     var events = this.modelFor('categories.category.activities.activity').get('events');
     var eventsArray = [];
     events.then(function(eventslist) {
       for (var i = 0; i < eventslist.length; i++) {
         eventsArray[i] = eventslist.currentState[i]._data;
-        eventsArray[i].url = "/categories/" + category + "/activities/" + activity + "/events/" + eventslist.currentState[i].id;
+        eventsArray[i].identifier = eventslist.currentState[i].id;
       }
       Ember.set(model, 'eventsArray', eventsArray);
     });
     Ember.set(model, 'events', events);
     return model;
+  },
+
+  actions: {
+      selectEvent(event) {
+          this.transitionTo("categories.category.activities.activity.events.event", event.identifier);
+      }
   }
 });
