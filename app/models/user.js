b8e37fbc6a9804c54f8a0342c1e8ad19383f9fcd
Alec Fenichel <alec.fenichel@gmail.com>
2016-04-19 18:25:45 -0400

    Add username to model

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  username: DS.attr('string'),
   auth: DS.belongsTo('auth', { async: true }),
   events: DS.hasMany('event', { async: true })
 });
